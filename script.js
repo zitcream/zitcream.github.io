e3a652da5620e6a73d15ff4b303e240a44149c10
Danny <joeboblovebabycakes@gmail.com>
2015-12-04 17:25:59 +0000

    ping

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function nothing () {
     alert("This button still doesn't do much.");
 }
+
+function ping () {
+    if(confirm("Ping")) {
+        if(confirm("Pong")) {
+            ping();
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
+}
